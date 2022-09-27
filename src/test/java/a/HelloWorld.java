6e93485d3d4893e24bad502654769fd1eac15b24
COMPUTER <msreddy0119@gmail.com>
2022-09-27 16:38:54 +0100

    updated changes to look in git bash

---
+++ src/test/java/a/HelloWorld.java
@@ -0,0 +1,34 @@
+package a;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class HelloWorld {
+
+    @Given("^User should be logged in and should be present at its own wall$")
+    public void user_should_be_logged_in_and_should_be_present_at_its_own_wall() throws Throwable {
+        System.out.println("User should be logged in and should be present at its own wall");
+        System.out.println("Changes for Pull in git bash");
+    }
+
+    @When("^I type the message in the text box$")
+    public void i_type_the_message_in_the_text_box() throws Throwable {
+        System.out.println("I type the message in the text box");
+    }
+
+    @And("^Click on post button$")
+    public void click_on_post_button() throws Throwable {
+        System.out.println("Click on post button");
+    }
+
+    @Then("^the message should get posted$")
+    public void the_message_should_get_posted() throws Throwable {
+        System.out.println("the message should get posted");
+        System.out.println("2nd changes video 32");
+        System.out.println("changed to look in git bash");
+    }
+
+
+}
